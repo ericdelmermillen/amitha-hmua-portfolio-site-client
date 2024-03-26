8c021de587d1df15f243fd2aeaf7440ce87dd846
Eric Millen <ericdelmermillen@gmail.com>
2024-03-26 13:42:37 -0400

    moved logic for shootsData to shoot as well as DeleteShootModal. Added pagination to shoots

--- src/components/Shoots/Shoots.jsx
+++ src/components/Shoots/Shoots.jsx
@@ -3,6 +3,7 @@ import './Shoots.scss';
 import Shoot from '../Shoot/Shoot';
 import AppContext from '../../AppContext';
 import DeleteShootModal from '../DeleteShootModal/DeleteShootModal.jsx'
+import { toast } from 'react-toastify';
 
 const Shoots = () => {
   const BASE_URL = import.meta.env.VITE_API_BASE_URL;
@@ -24,46 +25,66 @@ const Shoots = () => {
 
   const [ shootsData, setShootsData ] = useState([]);
   const [ showDeleteModal, setShowDeleteModal ] = useState(false)
+  const [ currentPage, setCurrentPage ] = useState(1);
+  const [ shouldUpdate, setShouldUpdate ] = useState(true);
+
+  const itemsPerPage = 2;
 
   useEffect(() => {
     const fetchShoots = async () => {
-      try {
-        const response = await fetch(`${BASE_URL}/shoots/all`);
+      if(shouldUpdate) {
+          
+        try {
+          const response = await fetch(`${BASE_URL}/shoots/all?page=${currentPage}&limit=${itemsPerPage}`);
 
-        if(response.ok) {
-          const data = await response.json()
-          setShootsData(data);
-        } else {
-          console.error('Failed to fetch shoots:', response.statusText);
+          if(response.ok) {
+            const data = await response.json()
+            setShootsData([...shootsData, ...data]);
+            setIsLoading(false); 
+            console.log(shootsData.length)
+            console.log(data.length)
+            if(data.length === 0) {
+              setShouldUpdate(false)
+            }
+          } else {
+            console.error('Failed to fetch shoots:', response.statusText);
+            toast.error(`Failed to fetch shoots:, ${response.statusText}`)
+          }
+          
+        } catch(error) {
+          console.log(`Error fetching shoots: ${error}`)
         }
-        
-      } catch(error) {
-        console.log(`Error fetching shoots: ${error}`)
       }
     }
     fetchShoots();
     console.log("shootsData fetched")
-  }, []);
-
+  }, [currentPage]);
   
   useEffect(() => {
     const handleScrollY = () => {
       const newScrollYPos = window.scrollY;
+      const documentHeight = document.documentElement.scrollHeight;
+      const windowHeight = window.innerHeight;
 
-      if(scrollYPos !== undefined && setPrevScrollYPos !== undefined && newScrollYPos !== scrollYPos) {
+      if (scrollYPos !== undefined && setPrevScrollYPos !== undefined && newScrollYPos !== scrollYPos) {
         setShowDeleteModal(false);
-        setSelectedShoot(null)
+        setSelectedShoot(null);
+      } else if (newScrollYPos + windowHeight >= documentHeight - 100) {
+        if (!isLoading) {
+          setCurrentPage((prevPage) => prevPage + 1);
+          setIsLoading(true); 
+        }
       }
     };
 
     handleScrollY();
 
-    window.addEventListener("scroll", handleScrollY);
+    window.addEventListener('scroll', handleScrollY);
 
     return () => {
-      window.removeEventListener("scroll", handleScrollY);
+      window.removeEventListener('scroll', handleScrollY);
     };
-  }, [scrollYPos]);
+  }, [scrollYPos, isLoading]);
 
   
 
