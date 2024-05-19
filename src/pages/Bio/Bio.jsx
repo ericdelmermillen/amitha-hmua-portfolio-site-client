b7566ac0adbb6508dbd4d889f523dfefdedac91d
ericdelmermillen <ericdelmermillen@gmail.com>
2024-05-19 17:29:26 -0400

    completed placeholders for bio page

--- src/pages/Bio/Bio.jsx
+++ src/pages/Bio/Bio.jsx
@@ -17,17 +17,8 @@ const Bio = () => {
       setIsComponentLoaded(true);
     }, minLoadingInterval);
     // }, 2000);
+    // }, 5000);
   };
-
-  // placeholder planning:
-  // --hero image and caption
-  // set specific heights for hero-container and hero-img relative to breakpoints
-  // container around image with position relative
-  // img and imgPlaceholder position absolute with z-index for placeholder higher
-  // conditional class on placeholder to transition the opacity to 0 or change the z-index after image is fully loaded
-  // same with hero-caption
-  // --text section
-  // render several fake paragraphs (3-4, with 3-6 lines each) at the top of the text container with the real paragraphs hidden with display none until hero-img loaded
   
   return (
     <>
@@ -62,13 +53,47 @@ const Bio = () => {
           </div>
           <div className="bio__divider"></div>
           <div className="bio__text-container">
-            <p className="bio__text">
+            <div className={`bio__text-placeholders ${componentIsLoaded 
+              ? "hide"
+              : ""}`
+            }>
+              <div className="bio__text-placeholder">
+                <div className="bio__placeholder-textLine"></div>
+                <div className="bio__placeholder-textLine"></div>
+                <div className="bio__placeholder-textLine"></div>
+                <div className="bio__placeholder-textLine"></div>
+                <div className="bio__placeholder-textLine bio__placeholder-textLine--50"></div>
+              </div>
+              <div className="bio__text-placeholder">
+                <div className="bio__placeholder-textLine"></div>
+                <div className="bio__placeholder-textLine"></div>
+                <div className="bio__placeholder-textLine"></div>
+                <div className="bio__placeholder-textLine bio__placeholder-textLine--75"></div>
+              </div>
+              <div className="bio__text-placeholder">
+                <div className="bio__placeholder-textLine"></div>
+                <div className="bio__placeholder-textLine"></div>
+                <div className="bio__placeholder-textLine"></div>
+                <div className="bio__placeholder-textLine bio__placeholder-textLine--25"></div>
+              </div>
+
+            </div>
+            <p className={`bio__text ${componentIsLoaded 
+              ? "show" 
+              : ""}`}
+            >
               Meet Amitha, a dynamic makeup artist and fashion stylist who thrives on celebrating the unique beauty of each person. With a deep understanding that beauty knows no bounds, she rejects the notion of a one-size-fits-all approach to makeup. Instead, she crafts bespoke experiences for her clients, considering their individuality, comfort levels, and personal style. 
             </p>
-            <p className="bio__text">
+            <p className={`bio__text ${componentIsLoaded 
+              ? "show" 
+              : ""}`}
+            >
               Located in the heart of Toronto, Ontario, Amitha's professional journey has been a whirlwind of diverse experiences within the beauty industry. Though she revels in all aspects of her craft, her passion ignites most brightly within the realms of Fashion and Bridal makeup. 
             </p>
-            <p className="bio__text">
+            <p className={`bio__text ${componentIsLoaded 
+              ? "show" 
+              : ""}`}
+            >
               From esteemed corporations to celebrated singers, actors, and brands across Canada, the USA, and the UK, Amitha and her team ensure that each client embarks on a unique and unforgettable beauty journey.
             </p>
           </div>
