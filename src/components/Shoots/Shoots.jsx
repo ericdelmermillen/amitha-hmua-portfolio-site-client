0422b7e6143e5f2483589a3d8f3c48037dc6b23c
Eric Delmer Millen <ericdelmermillen@Erics-MacBook-Pro.local>
2024-08-08 16:17:52 -0400

    pagination for non-filtered shoots seems to be working: need to test further

--- src/components/Shoots/Shoots.jsx
+++ src/components/Shoots/Shoots.jsx
@@ -8,6 +8,10 @@ import './Shoots.scss';
 
 const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
+const windowHeight = window.innerHeight;
+const fullHeight = document.body.scrollHeight;
+const overScrollThreshold = 300; 
+
 const Shoots = () => {
 
   const { shoot_id } = useParams();
@@ -27,7 +31,8 @@ const Shoots = () => {
     isInitialShootsLoad,
     setIsInitialShootsLoad,
     isOrderEditable, 
-    setIsOrderEditable
+    setIsOrderEditable,
+    isLoading
   } = useAppContext();
 
   const [ shouldUpdateAllShoots, setShouldUpdateAllShoots ] = useState(false);
@@ -36,40 +41,40 @@ const Shoots = () => {
   const location = useLocation();
   const navigate = useNavigate();
 
+  const { pathname, search } = location;
+    const currentURL = pathname.concat(search);
+
   const [ shootsData, setShootsData ] = useState([]);
   const [ currentPage, setCurrentPage ] = useState(1);
   const [ isOnShootDetails, setIsOnShootDetails ] = useState(location.pathname.includes('/shoot/'));
   const [ currentShootId, setCurrentShootId ] = useState(shoot_id);
-  // const [ isOrderEditable, setIsOrderEditable ] = useState(false);
 
   const [ activeDragShoot, setActiveDragShoot ] = useState({id: -1}); 
 
   // const itemsPerPage = 1;
   // const itemsPerPage = 2;
   // const itemsPerPage = 4;
-  // const itemsPerPage = 6;
+  const itemsPerPage = 6;
   // const itemsPerPage = 10;
   // const itemsPerPage = 12;
-  const itemsPerPage = 100;
+  // const itemsPerPage = 100;
 
   const [ finalPageLoaded, setFinalPageLoaded ] = useState(false);
-
+    
   const handleOverScroll = () => {
-    if(!finalPageLoaded) {
-      const windowHeight = window.innerHeight;
-      const fullHeight = document.body.scrollHeight;
-      const distanceToBottom = fullHeight - windowHeight - window.scrollY;
+    const distanceToBottom = fullHeight - (windowHeight + scrollYPos);
+
+    if(!finalPageLoaded && !isLoading) {
       
-      if(distanceToBottom <= (itemsPerPage * 600)) {
-        if(!selectedTag) {
-          setShouldUpdateAllShoots(true);
-        } else if(selectedTag) {
-          setShouldUpdateFilteredShoots(true);
-        }
+      if(distanceToBottom <= overScrollThreshold) {
+        setShouldUpdateAllShoots(true);
+        shootsData.length > 0 
+        ? setCurrentPage(c => c + 1)
+        : setCurrentPage(1);
       }
     }
   };
-    
+  
   const handleNewShootId = useCallback((shootId) => {
     setShootDetails(null);
     setCurrentShootId(shootId);
@@ -179,22 +184,24 @@ const Shoots = () => {
   
     setActiveDragShoot({id: -1});
   }, [activeDragShoot.id]);
-  
 
   // fetchAllShoots useEffect
   useEffect(() => {
-    console.log(currentPage)
-    if((!finalPageLoaded && !selectedTag && !location.search.includes("tag"))) {
+    let finalPageFetched = false;
+    
+    if(!finalPageLoaded && shouldUpdateAllShoots && !location.search.includes("tag")) {
+
       setIsLoading(true);
-      
+
       const fetchShoots = async () => {
       
         try {
           const response = await fetch(`${BASE_URL}/shoots/all?page=${currentPage}&limit=${itemsPerPage}`);
-
+          
           if(response.ok) {
             const { shootSummaries, isFinalPage } = await response.json();
-            setCurrentPage(currentPage => currentPage + 1);
+
+            finalPageFetched = isFinalPage;
             
             const data = shootSummaries;
             
@@ -205,13 +212,8 @@ const Shoots = () => {
               filteredData = data.filter(shoot => shoot.shoot_id !== +currentShoot);
             }    
             
-            if(isFinalPage) {
-              setTimeout(() => {
-                setFinalPageLoaded(true);
-              }, minLoadingInterval);
-            } else {
-              setShootsData(() => [...shootsData, ...filteredData]);
-            }
+            setShootsData(prevShoots => [...prevShoots, ...filteredData.filter(shoot => !prevShoots.some(prev => prev.shoot_id === shoot.shoot_id))]);
+
           }
         } catch(error) {
           console.log(`Error fetching shoots: ${error}`);
@@ -222,57 +224,66 @@ const Shoots = () => {
           setShouldUpdateAllShoots(false);
           setIsInitialShootsLoad(false);
           }, minLoadingInterval);
+
+          console.log(`isFinalPage: ${finalPageFetched}`)
+
+          if(finalPageFetched) {
+            setTimeout(() => {
+              setFinalPageLoaded(true);
+            }, minLoadingInterval);
+          } 
         }
       }
       fetchShoots();
     }
+    
+    setShouldUpdateAllShoots(false)
+  }, [shouldUpdateAllShoots, isOnShootDetails, currentPage, shoot_id, shootsData.length]);
 
-  }, [shouldUpdateAllShoots, shoot_id]);
+  // // fetchFilteredShoots useEffect
+  // useEffect(() => {    
+  //   if(!finalPageLoaded && selectedTag) {
+  //     setIsLoading(true);
 
-  // fetchFilteredShoots useEffect
-  useEffect(() => {    
-    if(!finalPageLoaded && selectedTag) {
-      setIsLoading(true);
+  //     const fetchFilteredShoot = async () => {
 
-      const fetchFilteredShoot = async () => {
+  //       try {
+  //         const response = await fetch(`${BASE_URL}/shoots/all?tag_id=${selectedTag.id}&page=${currentPage}&limit=${itemsPerPage}`);
 
-        try {
-          const response = await fetch(`${BASE_URL}/shoots/all?tag_id=${selectedTag.id}&page=${currentPage}&limit=${itemsPerPage}`);
+  //         if(response.ok) {
+  //           const { shootSummaries, isFinalPage } = await response.json();
+  //           const data = shootSummaries;
+  //           let filteredData = [...data];
+  //           setCurrentPage(currentPage => currentPage + 1);
 
-          if(response.ok) {
-            const { shootSummaries, isFinalPage } = await response.json();
-            const data = shootSummaries;
-            let filteredData = [...data];
-            setCurrentPage(currentPage => currentPage + 1);
+  //           if(isOnShootDetails) {
+  //             const currentShoot = shoot_id;
+  //             filteredData = data.filter(shoot => shoot.shoot_id !== +currentShoot);
+  //           }
 
-            if(isOnShootDetails) {
-              const currentShoot = shoot_id;
-              filteredData = data.filter(shoot => shoot.shoot_id !== +currentShoot);
-            }
+  //           setTimeout(() => {
+  //             setIsInitialShootsLoad(false);
+  //           }, minLoadingInterval);
 
-            setTimeout(() => {
-              setIsInitialShootsLoad(false);
-            }, minLoadingInterval);
-
-            if(isFinalPage) {
-              setFinalPageLoaded(true);
-            } else {
-              setShootsData(() => [...shootsData, ...filteredData]);
-            }
-          }
-        } catch(error) {
-          console.log(`Error fetching ${selectedTag.tag_name} shoots: ${error}`);
-          toast.error(`Failed to fetch ${selectedTag.tag_name} shoots:, ${error}`);
-        } finally {
-          setTimeout(() => {
-            setIsLoading(false); 
-            setShouldUpdateFilteredShoots(false);
-          }, minLoadingInterval);
-        }
-      }
-      fetchFilteredShoot();
-    }
-  }, [selectedTag, shouldUpdateFilteredShoots])
+  //           if(isFinalPage) {
+  //             setFinalPageLoaded(true);
+  //           } else {
+  //             setShootsData(() => [...shootsData, ...filteredData]);
+  //           }
+  //         }
+  //       } catch(error) {
+  //         console.log(`Error fetching ${selectedTag.tag_name} shoots: ${error}`);
+  //         toast.error(`Failed to fetch ${selectedTag.tag_name} shoots:, ${error}`);
+  //       } finally {
+  //         setTimeout(() => {
+  //           setIsLoading(false); 
+  //           setShouldUpdateFilteredShoots(false);
+  //         }, minLoadingInterval);
+  //       }
+  //     }
+  //     fetchFilteredShoot();
+  //   }
+  // }, [selectedTag, shouldUpdateFilteredShoots]);
 
   // handleOverScroll useEffect
   useEffect(() => {
@@ -283,8 +294,8 @@ const Shoots = () => {
 
   // useEffect to clear shoots state when navigating to page
   useEffect(() => {
-    const { pathname, search } = location;
-    const currentURL = pathname.concat(search);
+    // const { pathname, search } = location;
+    // const currentURL = pathname.concat(search);
       if(currentURL !== prevURL || showDeleteOrEditModal) {
 
         if(location.pathname.includes("shoot")) {
@@ -331,7 +342,7 @@ const Shoots = () => {
           {Array.from({ length: itemsPerPage }, (_, idx) => 
 
             <div 
-              className="shoots__placeholder"
+              className="shoots__placeholder initial"
               key={idx + 1}
             >
               <div className="shoots__placeholder-img"></div>
@@ -349,11 +360,11 @@ const Shoots = () => {
           : ""}`}
         >
 
-          {shootsData.map(shoot => (
+          {shootsData.map((shoot, idx) => (
             <Link 
               to={`/shoot/${shoot.shoot_id}`} 
               key={shoot.shoot_id}
-            >
+            >{idx + 1}
               <Shoot
                 key={shoot.shoot_id}
                 shoot_id={shoot.shoot_id}
@@ -370,7 +381,9 @@ const Shoots = () => {
               />
             </Link>
           ))}
+          {/* {!isInitialShootsLoad && !finalPageLoaded && (distanceToBottom <= overScrollThreshold) */}
 
+          {/* {!isInitialShootsLoad */}
           {!isInitialShootsLoad && !finalPageLoaded 
           
             ?
@@ -378,7 +391,7 @@ const Shoots = () => {
               (Array.from({ length: itemsPerPage }, (_, idx) => 
 
                 <div 
-                  className="shoots__placeholder"
+                  className="shoots__placeholder secondary"
                   key={idx + 1}
                 >
                   <div className="shoots__placeholder-img"></div>
