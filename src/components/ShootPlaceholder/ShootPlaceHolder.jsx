25cee6930962f7469894f6546009d5f52afd62c1
Eric Delmer Millen <ericdelmermillen@192.168.1.133>
2024-09-20 11:58:17 +0300

    moved shootPlaceholder jsx and styles into separate component

---
+++ src/components/ShootPlaceholder/ShootPlaceHolder.jsx
@@ -0,0 +1,17 @@
+import "./ShootPlaceHolder.scss"
+
+const ShootPlaceHolder = () => {
+  return (
+    <>
+      <div className="shootPlaceholder">
+        <div className="shootPlaceholder__placeholder-img"></div>
+        <div className="shootPlaceholder__placeholder-textContainer">
+          <div className="shootPlaceholder__placeholder-models"></div>
+          <div className="shootPlaceholder__placeholder-photographers"></div>
+        </div>
+      </div>
+      
+    </>
+  )};
+
+export default ShootPlaceHolder;
